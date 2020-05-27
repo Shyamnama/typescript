8e75b80b57d03778e7968a660a8b58a6e6b787d4
Shyam Nama <shyam.nama07@gmail.com>
2020-05-27 14:50:07 +0530

    master

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 const anchor = document.querySelector("a")!;
 
-console.log(anchor.href);
+// console.log(anchor.href);
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 console.log(form.children);
