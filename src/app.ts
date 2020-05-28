d305c33ccb1cd3bbf8687526fbb11d3bc0d85386
Shyam Kumar Nama <shyam.nama07@gmail.com>
2020-05-28 13:39:02 +0530

    master

--- src/app.ts
+++ src/app.ts
@@ -2,23 +2,18 @@
 
 // console.log(anchor.href);
 
-const form = document.querySelector(".new-item-form") as HTMLFormElement;
-console.log(form.children);
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// console.log(form.children);
 
 //inputs
 
-const type = document.querySelector("#type") as HTMLSelectElement;
-const toFrom = document.querySelector("#tofrom") as HTMLInputElement;
-const details = document.querySelector("#details") as HTMLInputElement;
-const amount = document.querySelector("#amount") as HTMLInputElement;
+const type = document.querySelector('#type') as HTMLSelectElement;
+const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
 
-form.addEventListener("submit", (e: Event) => {
+form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
 
-  console.log(
-    type.value,
-    toFrom.value,
-    details.value,
-    amount.value,
-  );
+  console.log(type.value, toFrom.value, details.value, amount.value);
 });
